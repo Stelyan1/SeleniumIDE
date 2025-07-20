81ed029f0d6048d1795f363e1f0870741db854f1
Stelyan1 <stilqnbliznakov@gmail.com>
2025-07-20 20:45:57 +0300

    Created yml file

--- obj/Release/net6.0/SeleniumIde.AssemblyInfo.cs
+++ obj/Release/net6.0/SeleniumIde.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("SeleniumIde")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bea9c42caa1abb60426316c5c91c4573e44923e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("SeleniumIde")]
